2eacd3830dbb007af5e6967a2e07aab80fb20533
Stefhon Roach <kingroach@Stefhons-MBP.lan>
2023-08-29 12:32:36 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -14,6 +14,29 @@ app.get('/greetings/:name', function(req, res) {
     res.send(`<Name>Hello, ${req.params.name}! It is so great to see you.</Name>`)
 })
 
+app.get('/tip', function(req, res) {
+    res.send('<h1>Are you ?</h1>')
+})
+
+app.get('/tip/:total/:tipPercentage', function(req, res) {
+    const total = parseFloat(req.params.total)
+    const tipPercentage = parseFloat(req.params.tipPercentage)
+    const tipAmount = (total * tipPercentage) / 100
+
+    res.send(`<h1>Thank you for eating at my restaurant! Your tip amount is $${tipAmount}</h1>`)
+})
+
+// app.get('/magic', function(req, res) {
+//     res.send('<h1>Eight Ball</h1>')
+// })
+
+app.get('/magic', function(req,res) {
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    let randomResponse = responses[Math.floor(Math.random()*responses.length)]
+
+    res.send(`<h1>Will I be a Millionaire? ${randomResponse}</h1>`)
+})
 
 app.listen(3000, function () {
     console.log('Listening on port 3000')
