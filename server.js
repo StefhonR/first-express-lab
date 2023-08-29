276f60f9212dd446beaecd2eb24089f2421e9b1d
Stefhon Roach <kingroach@Stefhons-MBP.lan>
2023-08-29 12:38:26 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -26,16 +26,12 @@ app.get('/tip/:total/:tipPercentage', function(req, res) {
     res.send(`<h1>Thank you for eating at my restaurant! Your tip amount is $${tipAmount}</h1>`)
 })
 
-// app.get('/magic', function(req, res) {
-//     res.send('<h1>Eight Ball</h1>')
-// })
-
-app.get('/magic', function(req,res) {
+app.get('/magic/:question', function(req,res) {
     const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
     let randomResponse = responses[Math.floor(Math.random()*responses.length)]
 
-    res.send(`<h1>Will I be a Millionaire? ${randomResponse}</h1>`)
+    res.send(`<h1>${req.params.question}: ${randomResponse}</h1>`)
 })
 
 app.listen(3000, function () {
